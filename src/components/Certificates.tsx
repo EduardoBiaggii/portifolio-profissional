7d95814600c87546e1041abb581c4851ef99696e
eduardobiaggii <paulo.biaje@hotmail.com>
2026-06-03 21:39:27 -0300

    feat: adicionar logo da estacio na secao de certificados

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -1,12 +1,23 @@
 import './Certificates.css';
+import estacioLogo from '../assets/estacio.png';
 
 const certs = [
+  {
+    course: 'Análise e Desenvolvimento de Sistemas',
+    institution: 'Estácio',
+    year: '5º Período',
+    area: 'Tecnologia da Informação',
+    icon: null,
+    logo: estacioLogo,
+    color: '#34d399',
+  },
   {
     course: 'Express Course',
     institution: 'Code.org',
     year: '2021',
     area: 'Ciência da Computação',
     icon: '💻',
+    logo: null,
     color: '#00adef',
   },
   {
@@ -15,6 +26,7 @@ const certs = [
     year: 'Abr 2023',
     area: 'Lógica de Programação · 40h',
     icon: '⚙️',
+    logo: null,
     color: '#a78bfa',
   },
 ] as const;
@@ -34,7 +46,12 @@ export default function Certificates() {
             className="cert__card"
             style={{ ['--cert-color' as any]: c.color }}
           >
-            <div className="cert__icon">{c.icon}</div>
+            <div className="cert__icon">
+              {c.logo
+                ? <img src={c.logo} alt={c.institution} className="cert__logo" />
+                : c.icon
+              }
+            </div>
             <div className="cert__body">
               <span className="cert__course">{c.course}</span>
               <span className="cert__institution">{c.institution}</span>
