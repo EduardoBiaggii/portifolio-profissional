1294de3273a283bd8332e2e1aa7949ad56c5edf2
eduardobiaggii <paulo.biaje@hotmail.com>
2026-06-03 22:15:33 -0300

    fix: remove base path para Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    // base: '/portifolio-profissional/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
