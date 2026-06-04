3aa9c4acbfe3052db0fa8a1d03ffdc263c5aa5e9
eduardobiaggii <paulo.biaje@hotmail.com>
2026-06-03 22:14:28 -0300

    ci: comentar base path no vite.config.ts para deploy no Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/portifolio-profissional/',
+    // base: '/portifolio-profissional/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
